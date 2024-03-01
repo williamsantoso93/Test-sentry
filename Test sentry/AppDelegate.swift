bb1fe198366b04899c2383834acb80bb2b80edb8
William Santoso <williamsantoso93@yahoo.com>
2024-03-01 11:29:57 +0700

    add sentry

--- Test sentry/AppDelegate.swift
+++ Test sentry/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Sentry
+//import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +15,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        FirebaseApp.configure()
+        
+        SentrySDK.start { options in
+            options.dsn = "https://2982b027f38c0175cdfb78dfb9cea1c4@o4506783954829312.ingest.sentry.io/4506783955681280"
+//            options.debug = true  //Enabled debug when first installing is always helpful
+            options.enableTracing = true 
+
+            // Uncomment the following lines to add more data to your events
+            // options.attachScreenshot = true // This adds a screenshot to the error events
+            // options.attachViewHierarchy = true // This adds the view hierarchy to the error events
+            options.environment = "staging"
+            options.attachViewHierarchy = true
+            options.enablePreWarmedAppStartTracing = true
+            options.enableMetricKit = true
+            options.enableTimeToFullDisplayTracing = true
+            options.swiftAsyncStacktraces = true
+        }
+        // Remove the next line after confirming that your Sentry integration is working.
+//        SentrySDK.capture(message: "This app uses Sentry! :)")
+
         // Override point for customization after application launch.
         return true
     }
--- Test sentry/ViewController.swift
+++ Test sentry/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import Sentry
+//import Firebase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let button = UIButton(type: .roundedRect)
+        button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+        button.setTitle("Test Crash", for: [])
+        button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+        view.addSubview(button)
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        SentrySDK.capture(message: "test")
+    }
+    
+    
+    @IBAction func crashButtonTapped(_ sender: AnyObject) {
+//        Crashlytics.crashlytics().log("test crash")
+        let numbers = [0]
+        let _ = numbers[1]
+    }
 }
 
